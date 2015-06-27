90c672d6615565f056a063533425727965907562
Mickaël Andrieu <andrieu.travail@gmail.com>
2015-06-27 05:34:36 +0200

    First commit

---
+++ Player.php
@@ -0,0 +1,45 @@
+<?php
+namespace BackBee\Standard\Bundle\PlayerBundle;
+
+/*
+ * Copyright (c) 2011-2015 Lp digital system
+ *
+ * This file is part of LpPlayerBundle.
+ *
+ * BackBee is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BackBee is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BackBee. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use BackBee\Bundle\AbstractBundle;
+
+/**
+ * @author Mickaël Andrieu <mickael.andrieu@lp-digital.fr>
+ */
+class Player extends AbstractBundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function start()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function stop()
+    {
+
+    }
+}
